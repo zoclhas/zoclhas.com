70ed6a41247aadbe9d339f92034e4c7b26dd3f67
Zoclhas <zoclhas@gmail.com>
2023-06-13 15:13:10 +0400

    added a rgb vars for both primary and secondary

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ export default function Home() {
         <>
             <section
                 id="hero"
-                className="h-screen flex flex-col gap-4 justify-center"
+                className="h-screen flex flex-col gap-4 justify-center px-4"
             >
-                <h1 className="text-[clamp(2rem,8vw,20rem)]">Zoclhas</h1>
+                <h1 className="text-[clamp(2rem,8vw,20rem)] uppercase text-center">
+                    Zoclhas
+                </h1>
             </section>
         </>
     );
