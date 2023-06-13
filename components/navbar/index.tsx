e525301c130f3a00ea1d2f8b468c2f69a3e3afd2
Zoclhas <zoclhas@gmail.com>
2023-06-13 14:48:53 +0400

    Completed theme switch w/ backdrop

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { ThemeSwitch } from "./theme-switch";
-import { useEffect, useState } from "react";
 
 export const NavBar = () => {
     const [isLoaded, setIsLoaded] = useState<boolean>(false);
@@ -18,19 +19,21 @@ export const NavBar = () => {
     }
 
     return (
-        <nav className="h-[80px] fixed w-full p-4 flex gap-4 justify-between items-center px-12 bg-primary bg-opacity-30 backdrop-blur-lg max-xs:px-4">
-            <motion.div
-                initial={{ opacity: 0, translateY: 20 }}
-                animate={{ type: "spring", opacity: 1, translateY: 0 }}
-                whileHover={{ scale: 1.1, opacity: 0.7 }}
-                whileTap={{ scale: 0.9 }}
-            >
-                <Link href="/" className="font-bold text-2xl">
-                    zoclhas.com
-                </Link>
-            </motion.div>
+        <>
+            <nav className="h-[80px] fixed w-full p-4 flex gap-4 justify-between items-center px-12 bg-primary bg-opacity-30 backdrop-blur-lg max-xs:px-4 z-[1000]">
+                <motion.div
+                    initial={{ opacity: 0, translateY: 20 }}
+                    animate={{ type: "spring", opacity: 1, translateY: 0 }}
+                    whileHover={{ scale: 1.1, opacity: 0.7 }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <Link href="/" className="font-bold text-2xl">
+                        zoclhas.com
+                    </Link>
+                </motion.div>
 
-            <ThemeSwitch />
-        </nav>
+                <ThemeSwitch />
+            </nav>
+        </>
     );
 };
