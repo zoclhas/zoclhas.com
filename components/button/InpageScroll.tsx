d8d6f630eb5424ebb6dd57fce7bcd236cb9943a5
Zoclhas <zoclhas@gmail.com>
2023-06-13 18:28:19 +0400

    some changes to hero

---
+++ components/button/InpageScroll.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react";
+import { motion, HTMLMotionProps } from "framer-motion";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
+
+interface ButtonProps extends HTMLMotionProps<"a"> {
+    fill?: boolean;
+    href: string;
+}
+
+export const InpageScroll: FC<ButtonProps> = ({
+    children,
+    fill,
+    href,
+    ...props
+}: ButtonProps) => {
+    const { scroll } = useLocomotiveScroll();
+
+    const buttonProps = {
+        whileHover: { scale: 1.1 },
+        whileTap: { scale: 0.9 },
+        className: `px-6 py-2 ${
+            fill ? "bg-[rgb(var(--secondary-rgb),0.2)]" : ""
+        } rounded-xl cursor-pointer block text-center`,
+        ...props,
+    };
+
+    return (
+        <motion.a
+            href={href}
+            {...buttonProps}
+            onClick={(e) => {
+                e.preventDefault();
+                scroll.scrollTo(href, { duration: 350 });
+            }}
+        >
+            {children}
+        </motion.a>
+    );
+};
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,8 +1,9 @@
 import { motion } from "framer-motion";
 
 import { WavyText } from "@/components/wavy-text";
-import { GitHub } from "@/components/icons";
+import { Discord, GitHub, Twitter } from "@/components/icons";
 import { IconButton } from "@/components/icon-button";
+import { InpageScroll } from "../button/InpageScroll";
 
 export const Hero = () => {
     return (
@@ -17,12 +18,21 @@ export const Hero = () => {
                 </motion.p>
                 <WavyText
                     text="zoclhas"
-                    replay={true}
-                    className="text-[clamp(4.5rem,8vw,20rem)] uppercase leading-[clamp(4.5rem,8vw,20rem)]"
+                    replay
+                    className="text-[clamp(4rem,8vw,20rem)] uppercase leading-[clamp(4rem,8vw,20rem)]"
                     delay={1.5}
-                    onLoad={(e) => console.log("hi")}
+                    float
                 />
             </div>
+            <motion.h2 className="">
+                <WavyText
+                    text="Web Dev | Material Artist"
+                    replay
+                    className="text-lg font-normal max-sm:text-base"
+                    delay={1.6}
+                    duration={0.005}
+                />
+            </motion.h2>
             <div className="flex gap-4">
                 <motion.div
                     initial={{ opacity: 0, translateX: -40 }}
@@ -34,6 +44,7 @@ export const Hero = () => {
                         href="https://github.com/zoclhas"
                         title="GitHub"
                         target="_blank"
+                        rel="noreferrer"
                     >
                         <GitHub height={1.2} />
                     </IconButton>
@@ -43,8 +54,14 @@ export const Hero = () => {
                     animate={{ opacity: 1, translateY: 0 }}
                     transition={{ type: "spring", delay: 1.8 }}
                 >
-                    <IconButton fill>
-                        <GitHub height={1.2} />
+                    <IconButton
+                        fill
+                        href="https://twitter.com/zoclhas"
+                        title="GitHub"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Twitter height={1.2} />
                     </IconButton>
                 </motion.div>
                 <motion.div
@@ -52,11 +69,34 @@ export const Hero = () => {
                     animate={{ opacity: 1, translateX: 0 }}
                     transition={{ type: "spring", delay: 1.8 }}
                 >
-                    <IconButton fill>
-                        <GitHub height={1.2} />
+                    <IconButton
+                        fill
+                        href="https://discord.com/users/301347642682900481"
+                        title="GitHub"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Discord height={1.2} />
                     </IconButton>
                 </motion.div>
             </div>
+
+            <motion.div
+                initial={{ opacity: 0, translateY: 100 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                transition={{ type: "spring", delay: 2 }}
+                className="flex gap-4 absolute bottom-8 w-full justify-center flex-wrap max-xs:gap-2"
+            >
+                <InpageScroll href="#about" fill>
+                    About
+                </InpageScroll>
+                <InpageScroll href="#projects" fill>
+                    Projects
+                </InpageScroll>
+                <InpageScroll href="#contact" fill>
+                    Contact
+                </InpageScroll>
+            </motion.div>
         </>
     );
 };
