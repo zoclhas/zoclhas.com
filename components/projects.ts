9f0306ddd9eb5deb7e411ddd12e297de0feed3d4
Zoclhas <zoclhas@gmail.com>
2023-06-14 20:08:22 +0400

    stack + layout card styles

--- components/projects.ts
+++ components/projects.ts
@@ -8,6 +8,14 @@ export const projects = [
             site: "https://zaura.net/amulet",
             github: null,
         },
+        stack: [
+            "256x",
+            "Stylized",
+            "Procedural",
+            "Substance",
+            "PixelGraph",
+            "Photoshop",
+        ],
     },
     {
         coverImage: "/zochy-bio.png",
@@ -18,15 +26,6 @@ export const projects = [
             site: "https://zochy.xyz",
             github: "https://github.com/zoclhas/zochy-bio",
         },
-    },
-    {
-        coverImage: "/zochy-bio.png",
-        title: "zochy.xyz",
-        description:
-            "Amulet is a high-res __stylized__ POM/PBR resourcepack for Minecraft. Textures are made procedurally. And all parts of the pack is made [LabPBR](https://wiki.shaderlabs.org/wiki/LabPBR_Material_Standard) compliant so it has all the fancy SSS, porosity, AO and more!",
-        links: {
-            site: "https://zochy.xyz",
-            github: "https://github.com/zoclhas/zochy-bio",
-        },
+        stack: ["HTML/CSS/JS"],
     },
 ];
