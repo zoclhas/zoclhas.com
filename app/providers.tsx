704a73dd2fcad1576290d52dabe816337734a414
Zoclhas <zoclhas@gmail.com>
2023-10-02 23:11:07 +0400

    move navbar, footer, pageload out of main

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,9 @@
 import { ThemeProvider } from "next-themes";
 import { useRef } from "react";
 import { LocomotiveScrollProvider as RLSProvider } from "react-locomotive-scroll";
+import Footer from "@/components/footer";
+import { NavBar } from "@/components/navbar";
+import { PageLoad } from "@/components/page-load";
 
 function Providers({ children }: { children: React.ReactNode }) {
   const containerRef = useRef(null);
@@ -20,9 +23,12 @@ function Providers({ children }: { children: React.ReactNode }) {
         }}
         containerRef={containerRef}
       >
+        <PageLoad />
+        <NavBar />
         <main data-scroll-container ref={containerRef}>
           {children}
         </main>
+        <Footer />
       </RLSProvider>
     </ThemeProvider>
   );
