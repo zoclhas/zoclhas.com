3b21a6db9d743b9121789b7d130552e3f4337246
Zoclhas <zoclhas@gmail.com>
2023-10-02 23:09:11 +0400

    providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,21 @@
 import "./globals.scss";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+
+import Providers from "./providers";
+import { NavBar } from "@/components/navbar";
+import { PageLoad } from "@/components/page-load";
+import Footer from "@/components/footer";
+
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Zoclhas",
+  description:
+    "Web Dev | Material Artist - Heya! This is my portfolio site, checkout my projects and so on!",
+  openGraph: {
+    images: "https://zoclhas.com/meta-img.png",
+  },
+  themeColor: "#2c4141",
+};
 
 export default function RootLayout({
   // Layouts must accept a children prop.
@@ -9,7 +26,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>
+          <PageLoad />
+          <NavBar />
+          {children}
+          <Footer />
+        </Providers>
+      </body>
     </html>
   );
 }
