5b05a8624a31be44e21bbc0c09294430edaab05a
Zoclhas <zoclhas@gmail.com>
2023-10-03 19:24:18 +0400

    ignore

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,9 +15,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
+ children,
 }: {
   children: React.ReactNode;
 }) {
