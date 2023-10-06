86dd0fcc22b9073a51bd80908be55624bcaf5f65
Zoclhas <zoclhas@gmail.com>
2023-10-06 23:29:51 +0400

    remove page load

--- frontend/components/sections/hero.tsx
+++ frontend/components/sections/hero.tsx
@@ -15,7 +15,7 @@ export const Hero = () => {
         <motion.p
           initial={{ opacity: 0, translateY: 40 }}
           animate={{ opacity: 1, translateY: 0 }}
-          transition={{ type: "spring", delay: 1.5 }}
+          // transition={{ type: "spring", delay: 1.5 }}
         >
           Hi, I&apos;m
         </motion.p>
@@ -23,7 +23,7 @@ export const Hero = () => {
           text="zoclhas"
           replay
           className="text-[clamp(4rem,8vw,20rem)] uppercase leading-[clamp(4rem,8vw,20rem)]"
-          delay={1.5}
+          delay={0}
           float
         />
       </div>
@@ -31,14 +31,14 @@ export const Hero = () => {
         text="Web Dev | Material Artist"
         replay
         className="text-lg font-normal max-sm:text-base"
-        delay={1.6}
+        delay={0.1}
         duration={0.005}
       />
       <div className="max-xs:gap-2 flex gap-4">
         <motion.div
           initial={{ opacity: 0, translateX: -40, rotate: 45 }}
           animate={{ opacity: 1, translateX: 0, rotate: 0 }}
-          transition={{ type: "spring", delay: 1.8 }}
+          // transition={{ type: "spring", delay: 1.8 }}
         >
           <IconButton
             fill
@@ -53,7 +53,7 @@ export const Hero = () => {
         <motion.div
           initial={{ opacity: 0, translateY: 40 }}
           animate={{ opacity: 1, translateY: 0 }}
-          transition={{ type: "spring", delay: 1.8 }}
+          // transition={{ type: "spring", delay: 1.8 }}
         >
           <IconButton
             fill
@@ -68,7 +68,7 @@ export const Hero = () => {
         <motion.div
           initial={{ opacity: 0, translateX: 40, rotate: -45 }}
           animate={{ opacity: 1, translateX: 0, rotate: 0 }}
-          transition={{ type: "spring", delay: 1.8 }}
+          // transition={{ type: "spring", delay: 1.8 }}
         >
           <IconButton
             fill
@@ -87,7 +87,7 @@ export const Hero = () => {
         className="mt-4"
         initial={{ opacity: 0, translateY: 100 }}
         animate={{ opacity: 1, translateY: 0 }}
-        transition={{ type: "spring", delay: 2 }}
+        // transition={{ type: "spring", delay: 2 }}
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
       >
@@ -97,7 +97,7 @@ export const Hero = () => {
       <motion.div
         initial={{ opacity: 0, translateY: 100 }}
         animate={{ opacity: 1, translateY: 0 }}
-        transition={{ type: "spring", delay: 2.5 }}
+        // transition={{ type: "spring", delay: 2.5 }}
         className="max-xs:gap-2 absolute bottom-8 flex w-full flex-wrap justify-center gap-4"
       >
         <InpageScroll href="#about" fill>
