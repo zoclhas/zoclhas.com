35e45e9341658692316dba3d0196da03f5d79045
Zoclhas <zoclhas@gmail.com>
2023-10-07 01:09:09 +0400

    update image lightbox

--- frontend/components/render-content/image.tsx
+++ frontend/components/render-content/image.tsx
@@ -50,53 +50,51 @@ export const ImageLightbox = ({ node }: { node: Root }) => {
         className="aspect-video rounded-xl object-cover object-center cursor-pointer"
       />
 
-      {id.length > 1 && (
-        <AnimatePresenceWrapper isOpen={open}>
-          <motion.div
-            className="fixed inset-0 h-full w-full z-[3000] bg-[rgb(var(--primary-rgb),0.7)] flex flex-col justify-center items-center backdrop-blur-sm p-4 isolate gap-2"
-            onClick={(e) => {
-              if (e.currentTarget === e.target) {
-                handleClose();
-              }
-            }}
-            initial={{ opacity: 0 }}
-            exit={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            key="modal-bg"
+      <AnimatePresenceWrapper isOpen={open && id.length > 5}>
+        <motion.div
+          className="fixed inset-0 h-full w-full z-[3000] bg-[rgb(var(--primary-rgb),0.7)] flex flex-col justify-center items-center backdrop-blur-sm p-4 isolate gap-2"
+          onClick={(e) => {
+            if (e.currentTarget === e.target) {
+              handleClose();
+            }
+          }}
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          key="modal-bg"
+        >
+          <motion.img
+            width={node.value.width}
+            height={node.value.height}
+            src={rootUrl + node.value.url}
+            alt={node.value.alt}
+            layoutId={id}
+            className="max-w-[1200px] w-full aspect-video rounded-xl object-cover object-center shadow-[0px_16px_32px_8px_rgb(0,0,0,0.1)] z-[1]"
+          />
+          <motion.h3
+            initial={{ opacity: 0, translateY: 200 }}
+            exit={{ opacity: 0, translateY: 200 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            key="alt-text"
+            className="-z-[1] !font-normal"
           >
-            <motion.img
-              width={node.value.width}
-              height={node.value.height}
-              src={rootUrl + node.value.url}
-              alt={node.value.alt}
-              layoutId={id}
-              className="max-w-[1200px] w-full aspect-video rounded-xl object-cover object-center shadow-[0px_16px_32px_8px_rgb(0,0,0,0.1)] z-[1]"
-            />
-            <motion.h3
-              initial={{ opacity: 0, translateY: 200 }}
-              exit={{ opacity: 0, translateY: 200 }}
-              animate={{ opacity: 1, translateY: 0 }}
-              key="alt-text"
-              className="-z-[1] !font-normal"
-            >
-              {node.value.alt}
-            </motion.h3>
+            {node.value.alt}
+          </motion.h3>
 
-            <motion.button
-              id={id}
-              className="absolute right-4 top-4 grid items-center rounded-full bg-[rgb(var(--secondary-rgb),0.2)] p-2 backdrop-blur-md"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              onClick={handleClose}
-              initial={{ opacity: 0, translateY: -200 }}
-              exit={{ opacity: 0, translateY: -200 }}
-              animate={{ opacity: 1, translateY: 0 }}
-            >
-              <Cross fill="#fff" height={0.9} />
-            </motion.button>
-          </motion.div>
-        </AnimatePresenceWrapper>
-      )}
+          <motion.button
+            id={id}
+            className="absolute right-4 top-4 grid items-center rounded-full bg-[rgb(var(--secondary-rgb),0.2)] p-2 backdrop-blur-md"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={handleClose}
+            initial={{ opacity: 0, translateY: -200 }}
+            exit={{ opacity: 0, translateY: -200 }}
+            animate={{ opacity: 1, translateY: 0 }}
+          >
+            <Cross fill="#fff" height={0.9} />
+          </motion.button>
+        </motion.div>
+      </AnimatePresenceWrapper>
     </LayoutGroup>
   );
 };
