aa573167783120c4acd7d175a14d702784c1d205
Zoclhas <zoclhas@gmail.com>
2023-10-07 00:01:04 +0400

    Working on post page

--- frontend/app/writings/page.tsx
+++ frontend/app/writings/page.tsx
@@ -28,30 +28,34 @@ export default async function Writings({
       {posts.docs.map((post) => {
         if (!post.is_draft) {
           return (
-            <Link
-              href={`/writing/${post.slug}`}
-              key={post.id}
-              className="rounded-2xl relative bg-[rgb(var(--secondary-rgb),0.1)] p-4 hover:bg-[rgb(var(--secondary-rgb),0.15)] transition-colors ease-in group"
-            >
-              <h2 className="flex text-2xl justify-between gap-2">
-                {post.title}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="group-hover:translate-x-1 transition-transform ease-in"
-                >
-                  <path d="m9 18 6-6-6-6" />
-                </svg>
-              </h2>
-              <p>{post.subtitle}</p>
-            </Link>
+            <div key={post.id} className="flex flex-col">
+              <span className="ml-4 text-sm text-[rgb(var(--secondary-rgb),0.8)] font-bold">
+                {post.createdAt.slice(0, 10)}
+              </span>
+              <Link
+                href={`/writings/${post.slug}`}
+                className="rounded-2xl relative bg-[rgb(var(--secondary-rgb),0.1)] p-4 hover:bg-[rgb(var(--secondary-rgb),0.15)] transition-colors ease-in group"
+              >
+                <h2 className="flex text-2xl justify-between gap-2">
+                  {post.title}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="group-hover:translate-x-1 transition-transform ease-in"
+                  >
+                    <path d="m9 18 6-6-6-6" />
+                  </svg>
+                </h2>
+                <p>{post.subtitle}</p>
+              </Link>
+            </div>
           );
         }
       })}
