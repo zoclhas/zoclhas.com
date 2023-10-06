90ceb704333ba01b48e03baf1cad89bc17d8dcaa
Zoclhas <zoclhas@gmail.com>
2023-10-07 03:07:38 +0400

    update doman list

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: [
-    "http://localhost:3001",
-    "https://zoclhas.com",
-    "http://payload.zoclhas.com",
-  ],
+  images: {
+    domains: [
+      "http://localhost:3001",
+      "https://zoclhas.com",
+      "http://payload.zoclhas.com",
+    ],
+  },
 };
 
 module.exports = nextConfig;
