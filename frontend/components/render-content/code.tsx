61f198e8a1221ebf19f77ae4747071ee874f3f71
Zoclhas <zoclhas@gmail.com>
2023-10-07 15:23:29 +0400

    show line numbers toggle

--- frontend/components/render-content/code.tsx
+++ frontend/components/render-content/code.tsx
@@ -4,12 +4,12 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus as theme } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export const CodeBlock = (props: any) => {
-  const { language, code, id }: CodeBlockProps = props;
+  const { language, code, id, show_line_numbers }: CodeBlockProps = props;
   return (
     <SyntaxHighlighter
       language={language}
       style={theme}
-      showLineNumbers
+      showLineNumbers={show_line_numbers}
       lineProps={{ style: { wordBreak: "break-all", whiteSpace: "pre-wrap" } }}
       wrapLines={true}
       lineNumberStyle={{ minWidth: "2rem" }}
@@ -29,4 +29,5 @@ interface CodeBlockProps {
     | "bash";
   code: string;
   id: string;
+  show_line_numbers: boolean;
 }
