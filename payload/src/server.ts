9b4999c018a6474557ee890b4002ae44809406de
Zoclhas <zoclhas@gmail.com>
2023-10-09 21:01:00 +0400

    init mail

--- payload/src/server.ts
+++ payload/src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import payload from 'payload';
+import express from "express";
+import payload from "payload";
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 // Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin');
+app.get("/", (_, res) => {
+  res.redirect("/admin");
 });
 
 const start = async () => {
@@ -16,13 +16,29 @@ const start = async () => {
     mongoURL: process.env.MONGODB_URI,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+    email: {
+      transportOptions: {
+        host: process.env.SMTP_HOST,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+        port: 587,
+        secure: true,
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      fromName: process.env.FROM_NAME,
+      fromAddress: process.env.FROM_ADDRESS,
+    },
+  });
 
   // Add your own express routes here
 
   app.listen(process.env.PORT || 3010);
-}
+};
 
 start();
