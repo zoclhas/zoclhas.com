378ee8d74d47c52c095bb60cbe3b2c965a542d63
Zoclhas <zoclhas@gmail.com>
2023-10-10 19:06:40 +0400

    reading time

--- frontend/components/render-content/index.tsx
+++ frontend/components/render-content/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useRef } from "react";
+import useReadingTime from "use-reading-time";
 import { Component as content } from "./rich-text";
 import { CodeBlock } from "./code";
 
@@ -9,25 +10,36 @@ const components = { content };
 type Props = {
   layout: any;
   className?: string;
+  date: string;
 };
 
-const RenderBlocks: React.FC<Props> = ({ layout, className }) => (
-  <div>
-    {layout.map((block: any, i: number) => {
-      if (block.blockType === "code_block") {
-        return <CodeBlock {...block} key={i} />;
-      }
+const RenderBlocks: React.FC<Props> = ({ layout, className, date }) => {
+  const post = useRef(null);
+  const { readingTime, wordsCount } = useReadingTime(post);
 
-      // @ts-ignore
-      const Block: React.FC<any> = components[block.blockType];
+  return (
+    <div ref={post}>
+      <div className="flex justify-between font-bold text-[rgb(var(--secondary-rgb),0.8)]">
+        {date.slice(0, 10)}
+        <span>{readingTime} minutes</span>
+      </div>
 
-      if (Block) {
-        return <Block {...block} key={i} />;
-      }
+      {layout.map((block: any, i: number) => {
+        if (block.blockType === "code_block") {
+          return <CodeBlock {...block} key={i} />;
+        }
 
-      return null;
-    })}
-  </div>
-);
+        // @ts-ignore
+        const Block: React.FC<any> = components[block.blockType];
+
+        if (Block) {
+          return <Block {...block} key={i} />;
+        }
+
+        return null;
+      })}
+    </div>
+  );
+};
 
 export default RenderBlocks;
