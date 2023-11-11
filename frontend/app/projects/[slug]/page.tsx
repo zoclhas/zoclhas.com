1a05e75658ce74af7abbedb313c2d8d90c32b2f1
Zoclhas <zoclhas@gmail.com>
2023-11-11 11:10:12 +0400

    styling changes

--- frontend/app/projects/[slug]/page.tsx
+++ frontend/app/projects/[slug]/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Reveal } from "@/components/reveal";
 import RenderBlocks, { NormalRenderBlocks } from "@/components/render-content";
+import { motion } from "framer-motion";
 export async function generateMetadata(
   { params }: { params: { slug: string } },
   parent: ResolvingMetadata,
@@ -48,6 +49,23 @@ export default async function Project({
 
   return (
     <Reveal>
+      <LinkButton href="/projects" fill className="mb-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="-scale-x-100"
+        >
+          <path d="m9 18 6-6-6-6" />
+        </svg>
+        Back to Projects
+      </LinkButton>
       <div className="relative overflow-hidden rounded-2xl">
         <Image
           src={process.env.NEXT_PUBLIC_API + project.meta.image.url}
@@ -56,22 +74,52 @@ export default async function Project({
           width={project.meta.image.width}
           className="aspect-video w-full rounded-lg object-cover object-center"
         />
-        <div className="gradient-blur-project max-md:hidden">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <div className="bottom-0 left-0 z-[20] w-full bg-gradient-to-t from-[var(--primary)] max-sm:mt-4 sm:p-4 md:absolute">
+        <div className="bottom-0 left-0 z-[20] w-full bg-gradient-to-t from-[var(--primary)] to-[rgb(var(--primary-rgb),0.7)] backdrop-blur-lg max-sm:mt-4 sm:p-4 md:absolute">
           <h1 className="text-6xl max-sm:text-4xl">{project.title}</h1>
           <p className="text-xl max-sm:text-lg">{project.subtitle}</p>
         </div>
       </div>
 
+      <div className="mt-4 flex flex-col">
+        <h2>Technologies:</h2>
+        <div className="flex flex-wrap gap-2">
+          {project.stacks.map((stack) => (
+            <div
+              className="grow select-none rounded-lg bg-[rgb(var(--secondary-rgb),0.2)] px-6 py-2 text-center"
+              key={stack}
+            >
+              {stack}
+            </div>
+          ))}
+        </div>
+      </div>
+
       <div className="prose md:prose-lg mt-8">
         <NormalRenderBlocks layout={project.layout as any} />
+      </div>
+
+      <div className="mt-4 flex flex-col">
+        <h2>Links:</h2>
+        <div className="flex flex-col gap-2">
+          <div className="break-words rounded-lg bg-[rgb(var(--secondary-rgb),0.2)] px-6 py-2">
+            Visit the website here:{" "}
+            <Link href={project.link} target="_blank" className="underline">
+              {project.link}
+            </Link>
+          </div>
+          {project.git_link && (
+            <div className="select-none break-words rounded-lg bg-[rgb(var(--secondary-rgb),0.2)] px-6 py-2">
+              Visit the GitHub repo here:{" "}
+              <Link
+                href={project.git_link}
+                target="_blank"
+                className="underline"
+              >
+                {project.git_link}
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </Reveal>
   );
