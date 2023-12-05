044fe4f03fe301966c02e89abb1c6b5bf1db5d07
Zoclhas <zoclhas@gmail.com>
2023-12-05 20:07:32 +0400

    render-content

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,21 @@
 import { Button } from "@/components/button";
 import { Discord, GitHub, Twitter } from "@/components/icons";
-import Image from "next/image";
+import { NormalRenderBlocks } from "@/components/render-content";
 
-export default function Home() {
+import { HomeProps } from "@/payload-types";
+
+const getPosts = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/home`, {
+    method: "GET",
+    next: { revalidate: 60 },
+  });
+
+  return res.json();
+};
+
+export default async function Home() {
+  const { about, posts, projects }: HomeProps = await getPosts();
+
   return (
     <main>
       <section id="hero" className="flex justify-center pt-12 sm:pt-32">
@@ -25,26 +38,44 @@ export default function Home() {
                 <GitHub />
               </Button>
               <Button
-                href="https://github.com/zoclhas"
+                href="https://twitter.com/zoclhas"
                 target="_blank"
                 isIcon
                 className="twitter-button"
-                aria-label="Zoclhas's GitHub profile button"
+                aria-label="Zoclhas's Twitter profile button"
               >
                 <Twitter />
               </Button>
               <Button
-                href="https://github.com/zoclhas"
+                href="https://discord.com/users/301347642682900481"
                 target="_blank"
                 isIcon
                 className="discord-button"
-                aria-label="Zoclhas's GitHub profile button"
+                aria-label="Zoclhas's Discord profile button"
               >
                 <Discord />
               </Button>
             </div>
           </div>
         </div>
+      </section>
+
+      <section id="about" className="mt-32">
+        <h1 className="text-3xl font-semibold">About</h1>
+        <div className="mt-4 text-xl">
+          <NormalRenderBlocks layout={about.content} />
+        </div>
+        <h3 className="mt-4 text-xl font-medium">Technologies:</h3>
+        <ul className="mt-1 flex flex-wrap gap-1">
+          {about.tech.map((t, i) => (
+            <li
+              key={i}
+              className="flex grow items-center justify-center rounded-md bg-gradient-to-b from-gray-100 to-gray-200/70 px-4 py-2 text-center dark:from-gray-900 dark:to-gray-900/50"
+            >
+              {t}
+            </li>
+          ))}
+        </ul>
       </section>
     </main>
   );
