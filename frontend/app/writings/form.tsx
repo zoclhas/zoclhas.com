2462659a779f427cd77f2ba87232c2a05666332c
zoclhas <zoclhas@gmail.com>
2024-07-18 13:30:23 +0800

    Completed signup form

---
+++ frontend/app/writings/form.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { CornerDownLeft } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export const Form = ({ disabled }: { disabled: boolean }) => {
+  const [input, setInput] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const validRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  return (
+    <>
+      <strong className="font-medium">Email:</strong>
+      <input
+        name="email"
+        type="email"
+        className="grow bg-amber-100 px-2 disabled:opacity-50 dark:bg-neutral-900"
+        placeholder="Enter your email..."
+        onChange={(e) => {
+          setInput(e.target.value);
+          setIsValid(validRegEx.test(input.toLowerCase()));
+        }}
+        disabled={disabled}
+        required
+      />
+      {isValid && !disabled && (
+        <button
+          className="h-5 bg-amber-200 px-2 dark:bg-neutral-800"
+          type="submit"
+        >
+          <CornerDownLeft className="size-4" />
+        </button>
+      )}
+    </>
+  );
+};
