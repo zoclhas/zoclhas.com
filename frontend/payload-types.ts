f0de4dc30da02e91fc72958e8575c4c7ae427206
zoclhas <zoclhas@gmail.com>
2024-07-18 13:02:32 +0800

    Start writings page

--- frontend/payload-types.ts
+++ frontend/payload-types.ts
@@ -218,3 +218,16 @@ export interface HomeProps {
     docs: Project[];
   };
 }
+
+export interface Doc<T> {
+  docs: T[];
+  totalDocs: number;
+  limit: number;
+  totalPages: number;
+  page: number;
+  pagingCounter: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: any;
+  nextPage: any;
+}
