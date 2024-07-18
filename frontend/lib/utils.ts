60e75d4539f0bc657250f1f840de5fe446a05d11
zoclhas <zoclhas@gmail.com>
2024-07-18 11:37:22 +0800

    Completed  section

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: Date | string): string {
+  if (!(date instanceof Date)) {
+    date = new Date(date);
+  }
+
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  let formattedDay = day < 10 ? "0" + day : day.toString();
+  let formattedMonth = month < 10 ? "0" + month : month.toString();
+
+  return `${formattedDay}-${formattedMonth}-${year}`;
+}
